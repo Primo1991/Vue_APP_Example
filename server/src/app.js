e52a88c8e60ec7cc78da55fa8ba21ab27da779e0
Primo1991 <dev@przemyslawchmiel.com>
2019-04-09 22:58:41 +0100

    server setup

--- server/src/app.js
+++ server/src/app.js
@@ -1,1 +1,18 @@
 console.log('hello')
+const express = require('express')
+const bodyParsel = require('body-parser')
+const cors = require('cors')
+const morgan = require('morgan')
+
+const app = express()
+app.use(morgan('combine'))
+app.use(bodyParsel.json())
+app.use(cors())
+
+app.get('/status', (req, res) => {
+  res.send({
+    message: 'hello world'
+  })
+})
+
+app.listen(process.env.PORT || 8082)
